f79305982c3199805cfeffef1d623a0b39315f10
dr fev <chimichanga2007@mail.ru>
2023-10-05 13:50:29 +0300

    Добавьте файлы проекта.

---
+++ Nail_Butyakov_HW-4/Tlab-5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Nail_Butyakov_HW_4
+{
+    internal class Program
+    {
+        static int mxnm(int a, int b)
+        {
+            return Math.Max(a, b);
+        }
+        static void Main(string[] args)
+        {
+
+            Console.Write("Упражнение 5.1 вывод наибольшее=го из двух чисел\nВведите первое число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Наибольшее из двух чисел: { mxnm(a, b)}");
+            Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+        }
+    }
+}
